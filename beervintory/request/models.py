42891881c2a5b05d354bb391c1f8235705d3fd55
Bryce Handerson <bryce.handerson@gmail.com>
2016-03-01 21:08:50 -0800

    delete all migrations

--- beervintory/request/models.py
+++ beervintory/request/models.py
@@ -7,6 +7,7 @@ from inventory import models as inv
 class Request(models.Model):
     number = models.SmallIntegerField(default = 0)
     #beer = models.ForeignKey(inv.Beer, null=True, unique=True)
-    beer = models.OneToOneField(inv.Beer, primary_key=True)
+    #beer = models.OneToOneField(inv.Beer, primary_key=True)
+    beer = models.CharField(max_length=30, null=True)
     def __str__(self):
             return "{0} ||  {1} request(s)".format(self.beer, self.number)

38df52a24ca4a16ad5441d6ec40160de12c112ef
Bryce Handerson <bryce.handerson@isilon.com>
2016-03-22 09:54:18 -0700

    inline models are awesome

--- beervintory/inventory/admin.py
+++ beervintory/inventory/admin.py
@@ -3,8 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from .models import Floor, Kegerator, Keg, Style, Brewer, Beer
 
-admin.site.register(Floor)
-admin.site.register(Kegerator)
+class KegeratorInline(admin.TabularInline):
+    model = Kegerator
+    extra = 0
+    show_change_link = True
+
+class KegInline(admin.TabularInline):
+    model = Keg
+    extra = 0
+    show_change_link = True
+
+class FloorAdmin(admin.ModelAdmin):
+    inlines = [
+            KegeratorInline,
+    ]
+
+class KegeratorAdmin(admin.ModelAdmin):
+    inlines = [
+            KegInline,
+    ]
+
+admin.site.register(Floor, FloorAdmin)
+admin.site.register(Kegerator, KegeratorAdmin)
 admin.site.register(Keg)
 admin.site.register(Style)
 admin.site.register(Brewer)

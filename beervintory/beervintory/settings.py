140e28b01a635b9c464a9cc58c9dc44776a6adff
Bryce Handerson <bryce.handerson@isilon.com>
2016-05-18 11:08:16 -0700

    added static files

--- beervintory/beervintory/settings.py
+++ beervintory/beervintory/settings.py
@@ -124,5 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'beervintory/static/beervintory')
+        ]
 STATIC_ROOT = "/var/www/beervintory/beervintory/static/"
 APPEND_SLASH = True

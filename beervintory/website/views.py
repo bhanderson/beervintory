51b3a04dbd89c9d4ce8b9f2d5060bdcde8222411
Bryce Handerson <bryce.handerson@gmail.com>
2016-08-29 23:13:44 -0700

    last updated

--- beervintory/website/views.py
+++ beervintory/website/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from inventory.models import Floor
+from django.contrib.auth.models import User
 
 def index(request):
     floors = Floor.objects.all()
-    context = {'Floors': floors}
+    logins = []
+    for user in User.objects.all():
+        logins.append(user.last_login)
+    updated = max(logins)
+    context = {'Floors': floors, 'Updated': updated}
     return render(request, 'website/index.html', context)
 
 # Create your views here.
